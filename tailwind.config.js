898f3f09f78a15b93225e01e50cc5cf1b4be7078
dashshreeansh10 <dashshreeansh10@gmail.com>
2026-07-05 18:52:42 +0530

    chore: core configuration files for production PWA

--- tailwind.config.js
+++ tailwind.config.js
@@ -57,20 +57,6 @@ export default {
         md: 'blur(12px)',
         lg: 'blur(20px)',
       },
-      fontSize: {
-        xs: ['0.75rem', { lineHeight: '1rem' }],
-        sm: ['0.875rem', { lineHeight: '1.25rem' }],
-        base: ['1rem', { lineHeight: '1.5rem' }],
-        lg: ['1.125rem', { lineHeight: '1.75rem' }],
-        xl: ['1.25rem', { lineHeight: '1.75rem' }],
-        '2xl': ['1.5rem', { lineHeight: '2rem' }],
-        '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
-        '4xl': ['2.25rem', { lineHeight: '2.5rem' }],
-      },
-      spacing: {
-        '128': '32rem',
-        '144': '36rem',
-      },
     },
   },
   plugins: [
